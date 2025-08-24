a2d4194d4cb577223b7220f6a015d7a590221f06
Ameya <ameya.n2007@gmail.com>
2025-08-23 21:13:51 -0700

    Enhance logging in latest-stable and utils.bash for better debugging

--- lib/utils.bash
+++ lib/utils.bash
@@ -26,6 +26,7 @@ sort_versions() {
 }
 
 list_github_tags() {
+	echo "Listing GitHub tags for $GH_REPO..."
 	git ls-remote --tags --refs "$GH_REPO" |
 		grep -o 'refs/tags/.*' | cut -d/ -f3- |
 		sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
